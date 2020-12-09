3c4d969c3b46b18e03ab14d7ca3dacf62bca3bee
Anson <ansonlit@live.ca>
2020-12-09 14:48:23 -0800

    implemented displaying of blurred photos

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,11 +1,11 @@
 let control = document.querySelector(".control")
 let thumb = document.querySelector("#thumb-bar")
-
-
-
+let submit = document.querySelector("button")
+let effect = document.querySelector("#effect")
+let image = document.querySelector(".displayed-img")
+let altarray = ["Eye", "Rock", "Purple Flowers", "Egyptian Art", "Butterfly"]
 
 function addImages() {
-    let altarray = ["Eye", "Rock", "Purple Flowers", "Egyptian Art", "Butterfly"]
     for (let i = 1; i <= 5; i++) {
         let img = document.createElement("img");
         img.src = `images/pic${i}.jpg`
@@ -16,14 +16,32 @@ function addImages() {
 addImages()
 
 
-thumb.addEventListener("mouseover", enlargephoto)
+thumb.addEventListener("mouseover", enlargePhoto)
 
 function showFormElements() {
     control.classList.remove("hidden")
 }
 
-function enlargephoto(evt) {
-    let image = document.querySelector(".displayed-img")
+function enlargePhoto(evt) {
     image.src = evt.target.src
+    console.log(image.src)
+    image.alt = evt.target.alt
     showFormElements()
+}
+
+
+submit.addEventListener("click", checkInput)
+
+function checkInput() {
+    let inputValue = effect.value
+    effect.value = ""
+    if (inputValue === "blur") {
+        for (let i = 1; i <= 5; i++) {
+            if (image.alt === altarray[i - 1]) {
+                image.src = `images/pic${i}B.jpg`
+            }
+        }
+    } else {
+        alert("Enter valid command")
+    }
 }
