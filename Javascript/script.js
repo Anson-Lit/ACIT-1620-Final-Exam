860e1a4c31708b247a99d8459f213ba4e5ec92ca
Anson <ansonlit@live.ca>
2020-12-09 12:59:08 -0800

    Add photos to thumb bar dynamically rather than inserting in html

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,2 +1,18 @@
 let control = document.querySelector(".control")
+let thumb = document.querySelector("#thumb-bar")
+
 control.classList.toggle("hidden")
+
+
+function looping() {
+    let altarray = ["Eye", "Rock", "Purple Flowers", "Egyptian Art", "Butterfly"]
+    for (let i = 1; i <= 5; i++) {
+        let img = document.createElement("img");
+        img.src = `images/pic${i}.jpg`
+        img.alt = altarray[i - 1]
+        thumb.appendChild(img)
+    }
+}
+
+
+looping()
